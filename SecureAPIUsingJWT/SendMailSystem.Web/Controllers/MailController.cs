745fe89734626b1d6c5ab5e60f3bcea2c42ec80e
Farhan Kaioum <km1472233@gmail.com>
2020-09-08 11:45:49 -0700

    mail sending system project using Mialkit and Mimekit

---
+++ SecureAPIUsingJWT/SendMailSystem.Web/Controllers/MailController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendMailSystem.Web.Models;
+using SendMailSystem.Web.Services;
+
+namespace SendMailSystem.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        private readonly IMailService _mailService;
+
+        public MailController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult> SendMail([FromForm] MailRequest request)
+        {
+            try
+            {
+                await _mailService.SendEmailAsync(request);
+                return Ok();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ SecureAPIUsingJWT/SendMailSystem.Web/Services/IMailService.cs
@@ -0,0 +1,10 @@
+﻿using SendMailSystem.Web.Models;
+using System.Threading.Tasks;
+
+namespace SendMailSystem.Web.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+    }
+}
