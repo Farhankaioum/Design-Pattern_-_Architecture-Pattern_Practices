cdfe134be888a83396080602c362591cd6410b8d
Farhan Kaioum <km1472233@gmail.com>
2020-09-11 11:11:18 -0700

    Extension method of DP added

---
+++ Onion/OnionArchitecture/Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddApplication();
+        }
+    }
+}
---
+++ Onion/OnionArchitecture/Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Product> Products { get; set; }
+    }
+}
