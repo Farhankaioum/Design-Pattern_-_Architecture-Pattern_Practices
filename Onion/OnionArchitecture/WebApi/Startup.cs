230feb637a23e4d6273103ca1b73f60a0f6a546a
Farhan Kaioum <km1472233@gmail.com>
2020-09-11 16:35:26 -0700

    api versioning added

--- Onion/OnionArchitecture/WebApi/Startup.cs
+++ Onion/OnionArchitecture/WebApi/Startup.cs
@@ -1,6 +1,7 @@
 using Application.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +31,19 @@ namespace WebApi
                     Version = "v1",
                     Title = "OnionArchitecture",
                 });
+            });
+            #endregion
+
+            #region API Versioning
+            // Add API Versioning to the Project
+            services.AddApiVersioning(config =>
+            {
+                // Specify the default API Version as 1.0
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                // If the client hasn't specified the API version in the request, use the default API version number 
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                // Advertise the API versions supported for the particular endpoint
+                config.ReportApiVersions = true;
             });
             #endregion
 
