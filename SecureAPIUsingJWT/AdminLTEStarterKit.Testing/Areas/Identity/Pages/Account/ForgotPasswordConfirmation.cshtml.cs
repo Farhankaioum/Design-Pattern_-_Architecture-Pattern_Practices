cc41b47b1b46b5fb3e3cd7a7dff7535d01000757
Farhan Kaioum <km1472233@gmail.com>
2020-09-13 13:20:14 -0700

    AdminLTEStarterKitTemplateImplemeted

---
+++ SecureAPIUsingJWT/AdminLTEStarterKit.Testing/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdminLTEStarterKit.Testing.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ForgotPasswordConfirmation : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
