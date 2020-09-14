16fbba61aa0d21bca8f3d7b232f2b93a79165c21
Farhan Kaioum <km1472233@gmail.com>
2020-09-14 15:56:32 -0700

    RoleManager, Role add, View added

---
+++ SecureAPIUsingJWT/CustomUserIdentityManagement/Models/UserRolesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomUserIdentityManagement.Models
+{
+    public class UserRolesViewModel
+    {
+       
+    }
+}
