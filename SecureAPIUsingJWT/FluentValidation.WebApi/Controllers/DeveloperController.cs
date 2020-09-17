fcd947be96d25e623c9c5a16a57dee0e0f624961
Farhan Kaioum <km1472233@gmail.com>
2020-09-17 11:50:23 -0700

    Fluent validation project added

---
+++ SecureAPIUsingJWT/FluentValidation.WebApi/Controllers/DeveloperController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation.WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluentValidation.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeveloperController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create(Developer developer)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SecureAPIUsingJWT/FluentValidation.WebApi/Validators/DeveloperValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidation.WebApi.Validators
+{
+    public class DeveloperValidator : AbstractValidator<Developer>
+    {
+        public DeveloperValidator()
+        {
+            RuleFor(p => p.FirstName)
+                .NotEmpty()
+                .WithMessage("{PropertyName} should not empty. Never!")
+                .Length(2, 25)
+                .Must(IsValidName).WithMessage("{PropertyName} Should be all letters");
+
+            RuleFor(p => p.Email)
+                .EmailAddress();
+        }
+
+        private bool IsValidName(string name)
+        {
+            return name.All(char.IsLetter);
+        }
+    }
+}
