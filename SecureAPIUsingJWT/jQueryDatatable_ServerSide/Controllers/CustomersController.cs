eeadd9f013cc6f8c98c1fb733fe4311a7669f652
Farhan Kaioum <km1472233@gmail.com>
2020-09-18 15:30:45 -0700

    DataTable with serverside project added

---
+++ SecureAPIUsingJWT/jQueryDatatable_ServerSide/Controllers/CustomersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace jQueryDatatable_ServerSide.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SecureAPIUsingJWT/jQueryDatatable_ServerSide/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using jQueryDatatable_ServerSide.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jQueryDatatable_ServerSide.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
