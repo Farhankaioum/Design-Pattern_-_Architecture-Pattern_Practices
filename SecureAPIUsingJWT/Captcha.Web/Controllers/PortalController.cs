b00f217bb0f87f3d59516abaf9feed0c9af04d8f
Farhan Kaioum <km1472233@gmail.com>
2020-10-29 18:31:01 +0600

    DNT Captcha added into project

---
+++ SecureAPIUsingJWT/Captcha.Web/Controllers/PortalController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Captcha.Web.Models;
+using DNTCaptcha.Core;
+using DNTCaptcha.Core.Providers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Captcha.Web.Controllers
+{
+    public class PortalController : Controller
+    {
+        private readonly IDNTCaptchaValidatorService _validatorService;
+
+        public PortalController(IDNTCaptchaValidatorService validatorService)
+        {
+            _validatorService = validatorService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateDNTCaptcha(
+            ErrorMessage = "Please Enter Valid Captcha",
+            CaptchaGeneratorLanguage = Language.English,
+            CaptchaGeneratorDisplayMode = DisplayMode.ShowDigits)]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!_validatorService.HasRequestValidCaptchaEntry(Language.English, DisplayMode.ShowDigits))
+                {
+                    ModelState.AddModelError(DNTCaptchaTagHelper.CaptchaInputName, "Please Enter Valid Captcha.");
+                    return View("Login");
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ SecureAPIUsingJWT/Captcha.Web/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Captcha.Web.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username Required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password Required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
