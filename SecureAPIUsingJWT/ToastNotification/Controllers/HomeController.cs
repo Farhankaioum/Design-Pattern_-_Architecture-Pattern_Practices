123ed86c399eeb88da07dc73902667371938a6d3
Farhan Kaioum <km1472233@gmail.com>
2020-12-04 23:47:00 +0600

    Toast notification configuration added

--- SecureAPIUsingJWT/ToastNotification/Controllers/HomeController.cs
+++ SecureAPIUsingJWT/ToastNotification/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,14 +13,17 @@ namespace ToastNotification.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly INotyfService _notyf;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, INotyfService notyf)
         {
             _logger = logger;
+            _notyf = notyf;
         }
 
         public IActionResult Index()
         {
+            _notyf.Success("Successfully Created");
             return View();
         }
 
--- SecureAPIUsingJWT/ToastNotification/Startup.cs
+++ SecureAPIUsingJWT/ToastNotification/Startup.cs
@@ -1,3 +1,4 @@
+using AspNetCoreHero.ToastNotification;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,13 @@ namespace ToastNotification
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddNotyf(config => 
+            { 
+                config.DurationInSeconds = 10;
+                config.IsDismissable = true;
+                config.Position = NotyfPosition.BottomRight; 
+            });
+
             services.AddControllersWithViews();
         }
 
